0fad8c613bf388a77d497ab273b077d93e5e7690
apollolarragoitia <apollolarragoitia@gmail.com>
2022-01-21 03:32:16 -0800

    Week 3 Lab Work
    Week 3 Lab Work that was not commited/pushed to main.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -7,16 +7,39 @@ import java.util.ArrayList;
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            int imagePointer = markdown.indexOf("!", currentIndex);
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            if (imagePointer == openBracket-1) {
+                currentIndex++;
+                continue;
+            
+            if (markdown.charAt(closeBracket+1) != ('(')){
+                currentIndex++;
+                continue;
+            }
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf (")", openParen);
+            if (openParen == -1 || closeParen == -1) {
+                break;
+            }
+            else {
+                toReturn.add(markdown.substring(openParen+1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+
+
+            /*
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
+            int openParen = markdown.indexOf("(", currentIndex);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+
+             */
         }
         return toReturn;
     }
--- test-file2.md
+++ test-file2.md
@@ -1,1 +1,7 @@
-[a link!](https://something.com)[
+[a link!](https://something.com) 
+
+(Non Link Use of Paran)
+[non link use of bracket]
+![image](https://testing.com/jpg)
+
+[another link!](https://test.com)
