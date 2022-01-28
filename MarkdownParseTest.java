a4779bbddcdd67f733255798af3bfdc85d45b186
apollolarragoitia <apollolarragoitia@gmail.com>
2022-01-28 06:29:36 -0800

    Fixing java.nio.file.NoSuchFileException
    Was recieivng java.nio.file.NoSuchFileException when attempting to run JUnit test. Figured out that the default directory for jUnit test in VSCode was an location that was not my current working directory. Temporarily set it directory of test files to local directories.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,6 +4,7 @@ import org.junit.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class MarkdownParseTest {
@@ -14,9 +15,17 @@ public class MarkdownParseTest {
 
     @Test
     public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("./test-file.md"));
+        String contents= Files.readString(Path.of("/Users/apollolarragoitia/Desktop/WINTER 2021 UCSD/cs15l/lab3/markdown-parse/test-file.md"));
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
-    
+
+    @Test
+    public void testFileExample() throws IOException{
+        String filename="test-file.md";
+        Path pathToFile = Paths.get(filename);
+        System.out.println(pathToFile.toAbsolutePath());
+        
+    }
+
 }
