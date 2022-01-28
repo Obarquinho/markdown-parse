66bcdb491a58a4806711090c5e3f5d443b68e1ef
apollolarragoitia <apollolarragoitia@gmail.com>
2022-01-28 08:54:37 -0800

    Symptom 0
    Recieved java.lang.Error: Unresolved compilation problem: when attempting to test file 1.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,51 +1,49 @@
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then take up to
+        // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            //int imagePointer = markdown.indexOf("!", currentIndex);
             int imagePointer = markdown.indexOf("!", currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf (")", openParen);
-            /*
-            if (markdown.charAt(closeBracket+1) != ('(')){
+            if (imagePointer == openBracket-1) {
+                currentIndex++;
                 continue;
             }
-            */
+            
+            if (markdown.charAt(closeBracket+1) != ('(')){
+                currentIndex++;
+                continue;
+            }
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf (")", openParen);
             if (openParen == -1 || closeParen == -1) {
                 break;
             }
             else {
                 toReturn.add(markdown.substring(openParen+1, closeParen));
+                currentIndex = closeParen + 1;
             }
-            currentIndex++;
-            System.out.println(closeParen);
+
+
+            /*
+            int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            int openParen = markdown.indexOf("(", nextCloseBracket);
+            int openParen = markdown.indexOf("(", currentIndex);
+            int closeParen = markdown.indexOf(")", openParen);
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
+
+             */
         }
-        System.out.println(markdown.charAt(40));
-        System.out.println(markdown.charAt(72));
         return toReturn;
-    }
-    
-
-    public static void main(String[] args) throws IOException {
-		/*
-        Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
-        */
-
-        String filename="test-file.md";
-        Path pathToFile = Paths.get(filename);
-        System.out.println(pathToFile.toAbsolutePath());
     }
 }
